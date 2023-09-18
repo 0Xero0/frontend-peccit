95bcda38c66367fad3914f83c6a37f85be6e9bd9
Jesid Polo <jesing482@gmail.com>
2023-09-17 21:04:26 -0500

    actualizando environment de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  urlBackend: 'http://172.16.3.104:5001',
+  urlBackend: 'http://172.16.1.134:5050',
   urlBackendVigia: 'http://172.16.3.104:5001',
   urlBackendArchivos: 'http://172.16.3.104:5001',
   tokenBackendArchivos: 'd4a32a3b-def6-4cc2-8f77-904a67360b53',
