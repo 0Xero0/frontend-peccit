fb50a5393e2c5d88bef0e2d419dd406c56b99b6f
padamego <padamego@gmail.com>
2024-10-30 17:30:00 -0500

    Atualizacion P 30-10-2024

--- src/app/services-menu-p/menu-header-p-service.ts
+++ src/app/services-menu-p/menu-header-p-service.ts
@@ -18,8 +18,8 @@ import { Injectable } from "@angular/core";
     {
         this.RutaActual=rutaActual;
         this.RutaModelo=rutaModelo;
-        //console.log(this.RutaActual)
-       // console.log(this.RutaModelo)
+        console.log(this.RutaActual)
+       console.log(this.RutaModelo)
     }
 
   }
