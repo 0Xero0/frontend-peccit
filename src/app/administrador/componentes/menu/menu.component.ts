b52c37970f481bd68bc4a5b4301631169cda26fe
Cesar <calfonsomora@gmail.com>
2024-11-06 09:37:26 -0500

    Actualización.

--- src/app/administrador/componentes/menu/menu.component.ts
+++ src/app/administrador/componentes/menu/menu.component.ts
@@ -5,6 +5,7 @@ import { Usuario } from 'src/app/autenticacion/modelos/IniciarSesionRespuesta';
 import { AutenticacionService } from 'src/app/autenticacion/servicios/autenticacion.service';
 import { Router } from '@angular/router';
 import { MenuHeaderPService } from 'src/app/services-menu-p/menu-header-p-service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-menu',
@@ -16,6 +17,10 @@ export class MenuComponent implements OnInit,AfterViewInit ,AfterViewChecked{
   usuario?: Usuario | null;
   isCollapsed = false;
   desplegado = true
+
+  inicioSesion: boolean = false
+  inicioVigia2: boolean = false
+
   urlMigaPan:string ="" //paolo
   Marcarmenu:boolean =false //paolo
   rutasMenu:any//paolo
@@ -40,7 +45,7 @@ export class MenuComponent implements OnInit,AfterViewInit ,AfterViewChecked{
     //this.ServiceMenuP.RutaModelo='/administrar/encuestas/1'
     //this.ActivarMenuP('/encuestas/1',0)
     //console.log('desdeinit')
-    
+
   }
 
   public abrir():void{
@@ -55,18 +60,18 @@ export class MenuComponent implements OnInit,AfterViewInit ,AfterViewChecked{
   {
     let carouselItem = document.querySelector(".ActiveP2");
     carouselItem?.classList.remove('ActiveP2');
-    
+
   }
   ngAfterContentChecked (){
-    
+
   }
   ngAfterViewInit()
   {
-    
+
   }
   ngAfterViewChecked()
   {
-    
+
     //console.log('desdeinit')
   }
   public SeleccionarMenuP(rutaModelo:string) :boolean
@@ -83,7 +88,7 @@ export class MenuComponent implements OnInit,AfterViewInit ,AfterViewChecked{
       this.urlMigaPan=this.router.routerState.snapshot.url
       //console.log(this.router.routerState.snapshot.url)
       return true
-      
+
     }
 
     return false
@@ -91,7 +96,7 @@ export class MenuComponent implements OnInit,AfterViewInit ,AfterViewChecked{
   public ActivarMenuP(rutaModelo:string) : boolean
   {
     //console.log(rutaModelo+ 'menup')
-    
+
     //for (let modulo of this.rutasMenu) {
       if(this.ServiceMenuP.RutaModelo===`/administrar${rutaModelo}`  )
       {
@@ -102,34 +107,39 @@ export class MenuComponent implements OnInit,AfterViewInit ,AfterViewChecked{
          ///console.log('--------------------')
         return true
       }
-      
+
     //}
-    
+
     //console.log('-----DESDE MENU FALSO---------------')
     return false
   }
   public MostrarNombrePanP() : string
-  {    
+  {
     let i:number=0
     for (let modulo of this.rutasMenu) {
       if(this.ServiceMenuP.RutaModelo===`/administrar${modulo.ruta}`)
-      {       
+      {
         this.ActivarMenuP(modulo.ruta)
-        return modulo.nombre         
-      }  
-      i+=1   
+        return modulo.nombre
+      }
+      i+=1
     }
-    
+
     //this.ServiceMenuP.RutaModelo='/administrar/encuestas/1'
     //this.router.navigate([this.ServiceMenuP.RutaModelo])
-    //this.ActivarMenuP('/encuestas/1',0) 
+    //this.ActivarMenuP('/encuestas/1',0)
     return 'Información General'
   }
 
   /************************************************************ */
   public cerrarSesion(){
-    this.servicioAutenticacion.cerrarSesion()
-    this.router.navigateByUrl('/inicio-sesion')
+    /* this.servicioAutenticacion.cerrarSesion()
+    this.router.navigateByUrl('/inicio-sesion') */
+    if(this.inicioVigia2){
+      window.location.href = environment.urlVigia2+'/administrar/administrar-aplicativos'
+    }else if(this.inicioSesion){
+      this.router.navigateByUrl('/inicio-sesion')
+    }
   }
   imprimirRuta(submodulo: Submodulo){
     //console.log(`/administrar${submodulo.ruta}`)
